9128eeded8abf25a4ac040d975caf54fa231c7f1
John Saunders <john@yettimedia.co.uk>
2018-02-28 19:04:10 +0000

    add_header Access-Control-Allow-Origin *;

--- src/Framework/Controller/Auth/AuthController.php
+++ src/Framework/Controller/Auth/AuthController.php
@@ -9,70 +9,70 @@ use TomWright\JSON\Exception\JSONEncodeException;
 use DevPledge\Application\Security\JWT\JWT;
 use DevPledge\Application\Security\JWT\Token;
 
-class AuthController
-{
+class AuthController {
 
-    /**
-     * @var JWT
-     */
-    private $jwt;
+	/**
+	 * @var JWT
+	 */
+	private $jwt;
 
-    /**
-     * AuthController constructor.
-     * @param JWT $jwt
-     */
-    public function __construct(JWT $jwt)
-    {
-        $this->jwt = $jwt;
-    }
+	/**
+	 * AuthController constructor.
+	 *
+	 * @param JWT $jwt
+	 */
+	public function __construct( JWT $jwt ) {
+		$this->jwt = $jwt;
+	}
 
-    /**
-     * @param Request $request
-     * @param Response $response
-     * @return Response
-     */
-    public function login(Request $request, Response $response)
-    {
-        $data = $request->getParsedBody();
+	/**
+	 * @param Request $request
+	 * @param Response $response
+	 *
+	 * @return Response
+	 */
+	public function login( Request $request, Response $response ) {
+		$data = $request->getParsedBody();
 
-        $username = $data['username'] ?? null;
-        $password = $data['password'] ?? null;
+		$username = $data['username'] ?? null;
+		$password = $data['password'] ?? null;
 
-        if ($username === 'tom' && $password === 'password') {
-            try {
-                $token = $this->jwt->generate((object)[
-                    'username' => $username,
-                    'password' => $password,
-                ]);
+		if ( $username === 'tom' && $password === 'password' ) {
+			try {
+				$token = $this->jwt->generate( (object) [
+					'name'     => 'Tommy Bum Bum',
+					'username' => $username,
+					'password' => $password,
+				] );
 
-                return $response->withJson(['token' => $token]);
-            } catch (JSONEncodeException $e) {
-                return $response->withJson(['error' => 'Could not generate token'], 500);
-            }
-        } else {
-            return $response->withJson(['error' => 'Invalid username or password'], 401);
-        }
-    }
+				return $response->withJson( [ 'token' => $token ] );
+			} catch ( JSONEncodeException $e ) {
+				return $response->withJson( [ 'error' => 'Could not generate token' ], 500 );
+			}
+		} else {
+			return $response->withJson( [ 'error' => 'Invalid username or password' ], 401 );
+		}
+	}
 
-    /**
-     * @param Request $request
-     * @param Response $response
-     * @return Response
-     */
-    public function refresh(Request $request, Response $response)
-    {
-        /**
-         * @var Token $token
-         */
-        $token = $request->getAttribute(Token::class);
+	/**
+	 * @param Request $request
+	 * @param Response $response
+	 *
+	 * @return Response
+	 */
+	public function refresh( Request $request, Response $response ) {
+		/**
+		 * @var Token $token
+		 */
+		$token = $request->getAttribute( Token::class );
 
-        try {
-            $newToken = $this->jwt->generate($token->getData());
-        } catch (JSONEncodeException $e) {
-            return $response->withJson(['error' => 'Could not generate token'], 500);
-        }
+		try {
+			$newToken = $this->jwt->generate( $token->getData() );
+		} catch ( JSONEncodeException $e ) {
+			return $response->withJson( [ 'error' => 'Could not generate token' ], 500 );
+		}
 
-        return $response->withJson(['token' => $newToken]);
-    }
+		return $response->withJson( [ 'token' => $newToken ] );
+	}
 
 }
