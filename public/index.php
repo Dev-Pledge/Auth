b1c03d7559b1fb54e388a2f4947becb1e95c37bb
John Saunders <john@yettimedia.co.uk>
2018-03-28 23:56:49 +0100

    dont use caching for now

--- public/index.php
+++ public/index.php
@@ -33,8 +33,6 @@ Integrations::addCommonSettings();
 Integrations::addCommonServices();
 Integrations::addCommonHandlers();
 
-$prodCacheDir = ( getenv( 'ENVIRONMENT' ) == 'production' ) ? __DIR__ . '/../data/integrations' : null;
-
 Integrations::addExtrapolations( [
 	new ExtrapolateSettings( __DIR__ . '/../src/Framework/Settings', "DevPledge\\Framework\\Settings" ),
 	new ExtrapolateServices( __DIR__ . '/../src/Framework/Services', "DevPledge\\Framework\\Services" ),
@@ -43,7 +41,7 @@ Integrations::addExtrapolations( [
 	new ExtrapolateControllerDependencies( __DIR__ . '/../src/Framework/ControllerDependencies', "DevPledge\\Framework\\ControllerDependencies" ),
 	new ExtrapolateFactoryDependencies( __DIR__ . '/../src/Framework/FactoryDependencies', "DevPledge\\Framework\\FactoryDependencies" ),
 	new ExtrapolateRouteGroups( __DIR__ . '/../src/Framework/RouteGroups', "DevPledge\\Framework\\RouteGroups" )
-], $prodCacheDir );
+] );
 
 Integrations::run();
 
