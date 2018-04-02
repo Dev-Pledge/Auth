7f5ba7e1a773f5849b184790aec1c0e356404746
John Saunders <john@yettimedia.co.uk>
2018-04-02 22:29:47 +0100

    fix

--- public/index.php
+++ public/index.php
@@ -37,6 +37,7 @@ Integrations::addCommonHandlers();
 Integrations::addExtrapolations( [
 	new ExtrapolateSettings( __DIR__ . '/../src/Framework/Settings', "DevPledge\\Framework\\Settings" ),
 	new ExtrapolateServices( __DIR__ . '/../src/Framework/Services', "DevPledge\\Framework\\Services" ),
+	new ExtrapolateServices( __DIR__ . '/../src/Application/Services', "DevPledge\\Application\\Services" ),
 	new ExtrapolateHandlers( __DIR__ . '/../src/Framework/Handlers', "DevPledge\\Framework\\Handlers" ),
 	new ExtrapolateRepositoryDependencies( __DIR__ . '/../src/Framework/RepositoryDependencies', "DevPledge\\Framework\\RepositoryDependencies" ),
 	new ExtrapolateControllerDependencies( __DIR__ . '/../src/Framework/ControllerDependencies', "DevPledge\\Framework\\ControllerDependencies" ),
